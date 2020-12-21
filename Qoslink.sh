e4e33484fc0148cf6388b40ac44340d7e9944210
Chefronpc <chefronpc@gmail.com>
2020-12-21 18:59:18 +0100

    Weryfikacja parametrów IP i konfiguracja interfejsów wewnątrz kontenerów
    Signed-off-by: Chefronpc <chefronpc@gmail.com>

--- Qoslink.sh
+++ Qoslink.sh
@@ -5,10 +5,10 @@ set -e
 
 # Zmienne Globalne
 # ----------------
-BRPREFIX="brlink"	# Domyślne nazwy bridgy, kontenerów oraz interfejsów
+BRPREFIX="brlink"	# Domyślne nazwy bridgy, linków oraz interfejsów
 BRMAX=512		# tworzonych w tych kontenerach.
-CNTPREFIX="qoslink"
-CNTMAX=256
+QOSPREFIX="qoslink"
+QOSMAX=256
 IFPREFIX="eth"
 IFMAX=32
 NETDEFAULT="10.1.1.0/24" # Domyślny adres sieci. Obsluga 254 sieci.
@@ -97,14 +97,14 @@ checkcontainer() {
 # --------------------------------
 freecontainer() {
   LISTCONTAINER=(`docker ps -a | sed -n -e '1!p' | awk '{ print $(NF) }' `)
-  for (( CNT=0; CNT<$CNTMAX; CNT++ )) ; do
+  for (( CNT=0; CNT<$QOSMAX; CNT++ )) ; do
     PASS=0 
-    ANS=(`echo ${LISTCONTAINER[@]} | grep $CNTPREFIX$CNT `)
+    ANS=(`echo ${LISTCONTAINER[@]} | grep $QOSPREFIX$CNT `)
     if [[ -n ${ANS[@]} ]] ; then
       PASS=1
     fi
     if [[ $PASS -eq 0 ]] ; then
-      CNTNAME=$CNTPREFIX$CNT 		# Wyszukana wolna nazwa dla nowego kontenera
+      QOSNAME=$QOSPREFIX$CNT 		# Wyszukana wolna nazwa dla nowego linka (kontenera)
       return 0
     fi
   done
@@ -131,7 +131,7 @@ checkinterface() {
 # --------------------------------
 freeinterface() {
   LISTINTERFACE=(`docker exec $1 ip a | awk -F': ' '{print $2}' | awk -F@ '/./ {print $1}' `)
-  for (( CNT=0; CNT<$CNTMAX; CNT++ )) ; do
+  for (( CNT=0; CNT<$IFMAX; CNT++ )) ; do
     PASS=0 
     for (( CNT2=0; CNT2<${#LISTINTERFACE[@]}; CNT2++ )) ; do
       if [[ "$IFPREFIX$CNT" = "${LISTINTERFACE[$CNT2]}" ]] ; then 
@@ -170,7 +170,7 @@ parseip() {
 # We - $3 1 Widoczność komunikatów,    0 - brak
 # ----------------------------
 comparenet() {
-  msg2 "Comprenet:   ====  $1  =   $2    ===="
+  msg2 "Porównanie dwóch adresów sieci  ($1) i ($2)"
   M1=(`echo $1 | awk -F/ '{print $2}' `)
   M2=(`echo $2 | awk -F/ '{print $2}' `)
   IP1=(`echo $1 | awk -F/ '{print $1}' | awk -F. '{print $1,$2,$3,$4}' `)
@@ -215,11 +215,11 @@ comparenet() {
 }
 
 
-# Sprawdza dostępnosc parametrów sieci
+# Sprawdza dostępnosc parametrów sieci w podanym kontenerze
 # We - $1 IP/Netmask
 # We - $2 nazwa kontenera
 # ------------------------------------
-checkip() {
+checkipcnt() {
   msg "Weryfikacja adresu IP $1 w kontenerze $2" 
   if parseip "$1" ; then
     LISTIP=(`docker exec $2 ip a | awk '/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\// {print $2,$(NF)}' `)
@@ -267,6 +267,62 @@ checkip() {
   fi
 }
 
+# Sprawdza dostępnosc parametrów sieci we wszystkich kontenerze
+# We - $1 IP/Netmask
+# ------------------------------------
+checkipall() {
+  msg "Weryfikacja adresu IP $1 we wszystkich kontenerach" 
+  if parseip "$1" ; then
+
+    STAT=1				# Domyslnie IP jest poza adresacja sieci w testowanym kontenerze
+    LISTCONTAINER=(`docker ps | sed -n -e '1!p' | awk '{ print $(NF) }' `)
+    for (( CNT2=0; CNT2<${#LISTCONTAINER[@]}; CNT2++ )) ; do
+      msg2 "="  # rem
+      msg2 "=============  kontener  ${LISTCONTAINER[$CNT2]}  ===================" # rem
+      LISTIP=(`docker exec ${LISTCONTAINER[$CNT2]} ip a | awk '/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\// {print $2,$(NF)}' `)
+#     echo ${LISTIP[@]}
+#     echo ${#LISTIP[@]}
+      let LISTIPMAX=${#LISTIP[@]}
+#     echo $LISTIPMAX
+      for (( CNT=0; CNT<$LISTIPMAX; CNT=$CNT+2 )) ; do
+  
+        if [[ "$1" = "${LISTIP[$CNT]}" ]] ; then
+          die 15 "Konflikt adresów IP $1 w kontenerze ${LISTCONTAINER[$CNT2]}"
+          return 		# Podane IP jest w konflikcie z IP w danym kontenerze
+        fi
+#        echo ----- $CNT:  $1   ----  ${LISTIP[$CNT]}    ------
+        comparenet "$1" "${LISTIP[$CNT]}"
+#        echo ++++ $NET:    $STAT
+        case "$NET" in
+          "3")
+#            echo "check case - podsieci zgodne IP bez konfliktu"
+            if [[ "$STAT" -lt "4" ]] ; then 
+              STAT=3
+            fi ;;
+          "2")
+#            echo "check case - podsieci rózne"
+            if [[ "$STAT" -lt "3" ]] ; then
+              STAT=2
+            fi ;;
+          "1")
+#            echo "check case - rozne dlugosci maski"
+            if [[ "$STAT" -lt "2" ]] ; then
+              STAT=1
+            fi ;;
+          "0")
+#            echo "check case - konflikt adresow"
+             die 15 "Konflikt adresów IP $1 w kontenerze ${LISTCONTAINER[$CNT2]}"
+            ;;
+        esac
+      done
+    done
+    msg "Brak konfliku adresu dla adresu $1"
+    return
+  elseif
+    die 12 "Niepoprawne dane lub format adresu sieci w funkcji <checkip>."
+  fi
+}
+
 # Zwraca wolny adres IP dla sieci zgodnej z zadanym IP
 # uwzgledniajac konfiguracje IP wszystkich kontenerow
 # ----------------------------------------------------
@@ -298,6 +354,12 @@ freeip() {
   fi
   if [[ -n ${CFG[6]} ]] ; then
     LISTIM[${#LISTIM[@]}]=${CFG[6]}
+  fi
+  if [[ -n ${CFG[23]} ]] ; then
+    LISTIM[${#LISTIM[@]}]=${CFG[23]}
+  fi
+  if [[ -n ${CFG[24]} ]] ; then
+    LISTIM[${#LISTIM[@]}]=${CFG[24]}
   fi
 
 # ----- Wyszukiwanie w kontenerach adresow IP zgodnych z siecią zadanego IP
@@ -358,8 +420,9 @@ freeip() {
 
   for (( CNT2=0; CNT2<$CNT2MAX; CNT2++ )) ; do
     msg2 " ====  Lista adresow IP używanych w danej podsieci  ====" # rem
-  #  msg2 "${LISTIM[@]}" # rem
-    echo "%%%%%%%%%%${LISTIM[@]}%%%%%%%%%%" # rem
+    if [[ ${CFG[22]} ]] ; then
+      echo "${LISTIM[@]}" # rem
+    fi
     msg2 " =========================================================================" # rem
     S=0  			# Status zmieni się jeżeli znajdzie się wolny 
          			# adres IP zakończy się zewnętrzna pętla CNT2
@@ -401,11 +464,11 @@ freeip() {
       CNT2=CNT2MAX
     fi
   done
-
+  NEWIP="$I0.$I1.$I2.$I3/$M1"
   msg2 " -----------------------------------" # rem
-  msg2 " WOLNY ADRES IP:  $I0.$I1.$I2.$I3/$M1" # rem
+  msg2 " WOLNY ADRES IP:  $NEWIP" # rem
   msg2 " -----------------------------------" # rem
-  
+  msg "Rezerwacja adresu IP: $NEWIP"
  return 0
 }
 
@@ -414,7 +477,7 @@ freeip() {
 set_c() {
 if [[ -z ${CFG[0]} ]] ; then
   if freecontainer ; then 
-    CFG[0]=$CNTNAME
+    CFG[0]=$QOSNAME
     msg "Przypisano nazwę kontenera -c: <${CFG[0]}>"
   fi
 fi
@@ -465,6 +528,46 @@ fi
 return 0
 }
 
+# -----  Przypisanie nazwy dla interfejsu <if3> w kontenerze linku <-c>  -------
+set_if3() {
+if [[ -z ${CFG[25]} ]] ; then
+  if freeinterface "${CFG[0]}" ; then
+    CFG[25]=$IFNAME
+    msg "Przypisano nazwę interfejsu -if3: <${CFG[25]}> w kontenerze <${CFG[0]}>"
+  fi
+fi
+return 0
+}
+
+# -----  Przypisanie nazwy dla interfejsu <if4> w kontenerze linku <-c>  -------
+set_if4() {
+if [[ -z ${CFG[26]} ]] ; then
+  if freeinterface "${CFG[0]}" ; then
+    CFG[26]=$IFNAME
+    msg "Przypisano nazwę interfejsu -if4: <${CFG[26]}> w kontenerze <${CFG[0]}>"
+  fi
+fi
+return 0
+}
+
+# -----  Uruchomienie kontenera łączącego hosty ( QoSLink )
+crt_c() {
+  docker run -d -ti --name ${CFG[0]} --hostname ${CFG[0]} --cap-add NET_ADMIN host:v1 /bin/bash
+  msg "Uruchomienie kontenera linka ${CFG[0]}"
+}
+
+
+
+# -----  Utworzenie linka bridga z kontenerem
+# ---------------------------------
+# bridge - kontener
+crt_link() {
+  pipework $1 -i $2 $3 $4
+  #pipework ${CFG[7]} -i ${CFG[3]} ${CFG[1]} ${CFG[5]}
+  msg "Polaczenie bridg'a -br1 <${CFG[7]}> z hostem <${CFG[1]}>"
+}
+
+
 # Weryfikacja wprowadzonych parametrów i ich zależności
 # -----------------------------------------------------
 # ---------------------------------------------------------------------------------------------
@@ -473,8 +576,8 @@ return 0
 # ---------------------------------------------------------------------------------------------
 #
 #   Tablica z dostępnymi opcjami oraz parametrami wejściowymi dla skyptu
-#   | 0 | 1  | 2  | 3  | 4  | 5  | 6  | 7  | 8  | 9    | 10   | 11   | 12   | 13    | 14    | 15  | 16 | 17 | 18 | 19 | 20    | 21 | 22 |
-WSK=(-c  -h1  -h2  -if1 -if2 -ip1 -ip2 -br1 -br2 -band1 -band2 -loss1 -loss2 -delay1 -delay2 -link -sw1 -sw2 -r1  -r2  -update -v   -V )
+#   | 0 | 1  | 2  | 3  | 4  | 5  | 6  | 7  | 8  | 9    | 10   | 11   | 12   | 13    | 14    | 15  | 16 | 17 | 18 | 19 | 20    | 21 | 22 | 23 | 24 | 25 | 26 )
+WSK=(-c  -h1  -h2  -if1 -if2 -ip1 -ip2 -br1 -br2 -band1 -band2 -loss1 -loss2 -delay1 -delay2 -link -sw1 -sw2 -r1  -r2  -update -v   -V   -ip3 -ip4 -if3 -if4 )
 # Kopiowanie parametrów do tablicy PARAM[]. Możliwe więcej niż 9 danych wejściowych.
 # ----------------------------------------------------------------------------------
 CNT=0
@@ -557,6 +660,19 @@ if [[ -n ${CFG[4]} ]] ; then
   fi
 fi
 
+# -----  Weryfikacja interfejsow  -----
+if [[ -n ${CFG[25]} ]] ; then
+  if checkinterface "${CFG[25]}" "${CFG[1]}" ; then
+    die 5 "Nazwa interfejsu z opcji -if3 ${CFG[25]} jest już utworzona w kontenerze ${CFG[1]}"
+  fi
+fi
+
+if [[ -n ${CFG[26]} ]] ; then
+  if checkinterface "${CFG[26]}" "${CFG[2]}" ; then
+    die 5 "Nazwa interfejsu z opcji -if4 ${CFG[26]} jest już utworzona w kontenerze ${CFG[2]}"
+  fi
+fi
+
 # ----  Weryfikacja poprawności IP1
 if [[ -n ${CFG[5]} ]] ; then
   if parseip ${CFG[5]}  ; then
@@ -602,12 +718,20 @@ case "$KOD" in
 240)						# h1 + ip1  ---  h2 + ip2  
     comparenet "${CFG[5]}" "${CFG[6]}" "1"
     if [[ "$NET" -eq "3" ]] ; then
-      checkip ${CFG[5]} ${CFG[1]}
-      checkip ${CFG[6]} ${CFG[1]}
-      checkip ${CFG[5]} ${CFG[2]}
-      checkip ${CFG[6]} ${CFG[2]}
+      checkipall ${CFG[5]}
+      checkipall ${CFG[6]}
       freeip ${CFG[5]}
+      CFG[23]=$NEWIP
       freeip ${CFG[6]}
+      CFG[24]=$NEWIP
+      set_c
+      set_br1
+      set_br2
+      set_if1
+      set_if2
+      set_if3
+      set_if4
+
     else
       exit 0
     fi
@@ -618,94 +742,15 @@ case "$KOD" in
     exit 0 ;;  
 esac
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Podgląd tablicy z parametrami
+# ---------------------
+for (( CNT=0; CNT<${#WSK[@]}; CNT++ )) ; do
+  echo "CFG[$CNT] -eq ${CFG[$CNT]} " 
+done
 
 echo Koniec
-
 exit 0
 
-
-#if checkip "${CFG[5]}" "${CFG[1]}"; then
-#  die 8 "Adres IP/Netmask z opcji -ip1 jest w konflikcie z pozostałymi adresami w kontenerze <${CFG[1]}>"
-#fi
-#
-#if checkip "${CFG[6]}" "${CFG[2]}"; then
-#  die 8 "Adres IP/Netmask z opcji -ip2 jest w konflikcie z pozostałymi adresami w kontenerze <${CFG[1]}>"
-#fi
-#
-
-
-# Ustalanie brakujących parametrów
-# --------------------------------
-
-# ---- Przypidsanie nazwy dla kontenera <c>
-if [[ -z ${CFG[0]} ]] ; then
-  if freecontainer ; then 
-    CFG[0]=$CNTNAME
-    msg "Przypisano nazwę kontenera -c: <${CFG[0]}>"
-  fi
-fi
-
-# -----  Przypisanie nazwy dla bridga <br1>  -------
-if [[ -z ${CFG[7]} ]] ; then
-  if freebridge ; then 
-    CFG[7]=$BRNAME
-    msg "Przypisano nazwę bridga -br1: <${CFG[7]}>"
-  fi
-fi
-
-# -----  Przypisanie nazwy dla bridga <br2>  -------
-if [[ -z ${CFG[8]} ]] ; then
-  if freebridge ; then 
-    CFG[8]=$BRNAME
-    msg "Przypisano nazwę bridga -br2: <${CFG[8]}>"
-  fi
-fi
-
-# -----  Przypisanie nazwy dla interfejsu <if1> w hoscie <-h1>  -------
-if [[ -z ${CFG[3]} ]] ; then
-  if freeinterface "${CFG[1]}" ; then
-    CFG[3]=$IFNAME
-    msg "Przypisano nazwę interfejsu -if1: <${CFG[3]}> w kontenerze <${CFG[1]}>"
-  fi
-fi
-
-# -----  Przypisanie nazwy dla interfejsu <if2> w hoscie  <-h2>  -------
-if [[ -z ${CFG[4]} ]] ; then
-  if freeinterface "${CFG[2]}" ; then
-    CFG[4]=$IFNAME
-    msg "Przypisano nazwę interfejsu -if2: <${CFG[4]}> w kontenerze <${CFG[2]}>"
-  fi
-fi
-
-########################################!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!    usunąć !!!!!!!!!!!!!!!!!!!!!!!
-# Podgląd tablicy CFG[]
-# ---------------------
-#for (( CNT=0; CNT<${#WSK[@]}; CNT++ )) ; do
-#  echo "CFG[$CNT] -eq ${CFG[$CNT]} " 
-#done
-
-
-# Uruchomienie kontenera łączącego hosty
-# --------------------------------------
-docker run -d -ti --name ${CFG[0]} --hostname ${CFG[0]} --cap-add NET_ADMIN host:v1 /bin/bash
-msg "Uruchomienie kontenera ${CFG[0]}"
 
 # Konfiguraacja poloczen kontenerow
 # ---------------------------------
